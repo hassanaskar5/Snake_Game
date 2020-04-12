db0af7e57d107314a28a6b32dd61813a2d78984b
Hassan Askar <hassanaskar5@gmail.com>
2020-04-13 00:03:21 +0100

    Added SnakeDrawBorders Function.

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void SnakeDrawBorders(void);
 int main()
 {
     printf("Hello world!\n");
+    SnakeDrawBorders();
     return 0;
+}
+void SnakeDrawBorders(void)
+{
+    printf("Draw Borders\n");
 }
